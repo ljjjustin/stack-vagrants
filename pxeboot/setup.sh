aee619353817ea3c3a911f4f9631ca8d24d3ce17
Jiajun Liu <iamljj@gmail.com>
2019-10-12 20:34:33 +0800

    pxeboot: update UEFI boot config

--- pxeboot/setup.sh
+++ pxeboot/setup.sh
@@ -16,8 +16,10 @@ ensure_packages dnsmasq syslinux nfs-utils
 systemctl enable dnsmasq nfs
 
 # setup directories
-mkdir -p /pxeboot/{images,pxelinux.cfg}
-cp -rf /usr/share/syslinux/{pxelinux.0,menu.c32,vesamenu.c32} /pxeboot
+TOPDIR=/pxeboot
+TFTPBOOT=${TOPDIR}/images
+mkdir -p ${TFTPBOOT}/pxelinux.cfg
+cp -rf /usr/share/syslinux/{pxelinux.0,menu.c32,vesamenu.c32} ${TFTPBOOT}
 
 # generate ssh key pair
 if [ ! -d ~/.ssh ]; then
@@ -29,9 +31,9 @@ fi
 
 # auto change work directory
 workdir=$(cd $(dirname $0) && pwd); cd ${workdir}
-if [ "${workdir}" != "/pxeboot" ]; then
-    cp -f * /pxeboot
-    exec /pxeboot/setup.sh
+if [ "${workdir}" != "${TOPDIR}" ]; then
+    cp -af images ks.template pxerc setup.sh ${TOPDIR}
+    exec ${TOPDIR}/setup.sh
 fi
 
 # config all components
@@ -45,31 +47,27 @@ dhcp-range=${DHCP_RANGE},${DHCP_NETMASK}
 dhcp-option=option:router,${DHCP_GATEWAY}
 
 enable-tftp
-tftp-root=/pxeboot
-
-dhcp-match=set:bios_mode,option:client-arch,0
-dhcp-boot=tag:bios_mode,pxelinux.0
-
-dhcp-match=set:efi_mode_v6,option:client-arch,6
-dhcp-boot=tag:efi_mode_v6,BOOTIA32.EFI
-
-dhcp-match=set:efi_mode_v7,option:client-arch,7
-dhcp-boot=tag:efi_mode_v7,BOOTX64.EFI
-
-dhcp-match=set:efi_mode_v9,option:client-arch,9
-dhcp-boot=tag:efi_mode_v9,BOOTX64.EFI
+tftp-root=${TFTPBOOT}
 
 # PXEClient:Arch:00000
-pxe-service=tag:bios_mode,X86PC, "Boot BIOS PXE", pxelinux
+dhcp-match=set:bios_mode,option:client-arch,0
+dhcp-boot=tag:bios_mode,pxelinux.0
+pxe-service=tag:bios_mode,X86PC,"Boot BIOS PXE",pxelinux
 
 # PXEClient:Arch:00006
-pxe-service=tag:efi_mode_v6, IA32_EFI, "Boot IA32_EFI PXE", BOOTIA32.EFI
+dhcp-match=set:uefi_mode_v6,option:client-arch,6
+dhcp-boot=tag:uefi_mode_v6,grubia32.efi
+pxe-service=tag:uefi_mode_v6,IA32_EFI,"Boot 32 Bits EFI PXE",grubia32.efi
 
 # PXEClient:Arch:00007
-pxe-service=tag:efi_mode_v7, BC_EFI, "Boot UEFI PXE-BC", BOOTX64.EFI
+dhcp-match=set:uefi_mode_v7,option:client-arch,7
+dhcp-boot=tag:uefi_mode_v7,grubx64.efi
+pxe-service=tag:uefi_mode_v7,BC_EFI,"Boot 64 Bits UEFI PXE",grubx64.efi
 
 # PXEClient:Arch:00009
-pxe-service=tag:efi_mode_v9, X86-64_EFI, "Boot UEFI PXE-64", BOOTX64.EFI
+dhcp-match=set:uefi_mode_v9,option:client-arch,9
+dhcp-boot=tag:uefi_mode_v9,grubx64.efi
+pxe-service=tag:uefi_mode_v9,X86-64_EFI,"Boot 64 Bits UEFI PXE",grubx64.efi
 EOF
 systemctl restart dnsmasq
 
@@ -94,15 +92,11 @@ cat > uefi_default <<  EOF
 set default="0"
 set timeout=60
 
-function load_video {
-  insmod efi_gop
-  insmod efi_uga
-  insmod video_bochs
-  insmod video_cirrus
-  insmod all_video
-}
-
-load_video
+insmod efi_gop
+insmod efi_uga
+insmod video_bochs
+insmod video_cirrus
+insmod all_video
 insmod gzio
 insmod part_gpt
 insmod ext2
@@ -119,35 +113,37 @@ echo -n > /etc/exports
 for iso in $(find /srv/ -name "*.iso" | sort)
 do
 	isofile=$(basename ${iso})
-	isodir=${isofile%.iso}
-	if [ ! -d ${isodir} ]; then
-		mkdir -p images/${isodir}/iso
+	isoname=${isofile%.iso}
+        isomountdir=${TFTPBOOT}/${isoname}/iso
+        ksconfig=${TFTPBOOT}/${isoname}/ks.cfg
+
+	if [ ! -d ${isomountdir} ]; then
+		mkdir -p ${isomountdir}
 	fi
 	if ! mount | grep -q -w "${isofile}"; then
-		mount -o loop ${iso} images/${isodir}/iso
+		mount -o loop ${iso} ${isomountdir}
 	fi
-	cp -f ks.template images/${isodir}/ks.cfg
-	sed -i "s|{{NFSHOST}}|${nfshost}|g" images/${isodir}/ks.cfg
-	sed -i "s|{{ISODIR}}|/pxeboot/images/${isodir}/iso|g" images/${isodir}/ks.cfg
-
+	cp -f ks.template ${ksconfig}
         pubkey=$(cat ~/.ssh/id_rsa.pub)
-        sed -i "s|{{SSHPUBKEY}}|${pubkey}|g" images/${isodir}/ks.cfg
+	sed -i "s|{{NFSHOST}}|${nfshost}|g" ${ksconfig}
+	sed -i "s|{{ISODIR}}|${isomountdir}|g" ${ksconfig}
+        sed -i "s|{{SSHPUBKEY}}|${pubkey}|g" ${ksconfig}
 
-	echo "/pxeboot/images/${isodir} *(rw,sync,no_subtree_check,all_squash)" >> /etc/exports
-	echo "/pxeboot/images/${isodir}/iso *(rw,sync,no_subtree_check,all_squash)" >> /etc/exports
+	echo "${TFTPBOOT}/${isoname} *(rw,sync,no_subtree_check,all_squash)" >> /etc/exports
+	echo "${isomountdir} *(rw,sync,no_subtree_check,all_squash)" >> /etc/exports
 
 	cat >> bios_default << EOS
-LABEL ${isodir}
-        MENU LABEL ${isodir}
-        kernel /images/${isodir}/iso/isolinux/vmlinuz
-        append initrd=/images/${isodir}/iso/isolinux/initrd.img ksdevice=bootif kssendmac ks=nfs:${nfshost}:/pxeboot/images/${isodir}/ks.cfg
+LABEL ${isoname}
+        MENU LABEL ${isoname}
+        kernel ${isomountdir}/isolinux/vmlinuz
+        append initrd=${isomountdir}/isolinux/initrd.img ks=nfs:${nfshost}:${ksconfig} ksdevice=bootif kssendmac
         ipappend 2
 EOS
 
 	cat >> uefi_default << EOS
-menuentry 'Install ${isodir}' {
-        linuxefi /images/${isodir}/iso/images/pxeboot/vmlinuz ksdevice=bootif kssendmac ks=nfs:${nfshost}:/pxeboot/images/${isodir}/ks.cfg
-        initrdefi /images/${isodir}/iso/images/pxeboot/initrd.img
+menuentry 'Install ${isoname}' {
+        linuxefi ${isomountdir}/images/pxeboot/vmlinuz ks=nfs:${nfshost}:${ksconfig} ksdevice=bootif kssendmac
+        initrdefi ${isomountdir}/images/pxeboot/initrd.img
 }
 
 EOS
@@ -158,8 +154,8 @@ cat >> bios_default << EOF
 MENU end
 EOF
 
-mv -f bios_default pxelinux.cfg/default
-mv -f uefi_default grub.cfg
+mv -f bios_default ${TFTPBOOT}/pxelinux.cfg/default
+mv -f uefi_default ${TFTPBOOT}/grub.cfg
 
 exportfs -au
 systemctl restart nfs
