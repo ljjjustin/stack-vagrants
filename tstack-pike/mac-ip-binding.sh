30cd6f11caaa9a1a88e8b27f7c98fd7aef1f4fa6
Liu Jiajun <iamljj@gmail.com>
2020-06-17 17:17:33 +0800

    remove gaiastack and tstack project

--- tstack-pike/mac-ip-binding.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 1 5)
-do
-    ip_postfix=$((110+i))
-    virsh net-update vagrant-libvirt add ip-dhcp-host "<host mac='52:54:56:00:01:0${i}' ip='192.168.121.${ip_postfix}'/>" --live --config --parent-index 0
-done
